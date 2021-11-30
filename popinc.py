d9284aa28d932abc6eb42283b96bd1da55d8bb9a
Pam Froemke <pam.froemke@usda.gov>
2021-11-30 13:16:50 -0700

    Create popinc.py

---
+++ popinc.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Load the popinc csv file
+df=pd.read_csv(r'E:\\GIS_Backup\\WaterDemand\\DataWaterDemand\\popinc_proj.csv')
+print(df)
+
+# Load the withdrawals data
+wd=pd.read_csv(r'E:\\GIS_Backup\\WaterDemand\\DataWaterDemand\\wd2015.csv')
+print(wd)
+
+wpu
+# Get just the data for 2015
+'year'=2015
