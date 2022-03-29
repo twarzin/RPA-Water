6d814d38d81801e9d2b40c91fe10f86394cb078b
twarzin <twwarziniack@fs.fed.us>
2022-03-29 15:45:50 -0600

    Update estimateGrowth.r

--- estimateGrowth.r
+++ estimateGrowth.r
@@ -40,18 +40,27 @@ us2015$YEAR = 2015
 # yes: 2015, 2010, 1995, 1990
 # no: 2000
 
+# rename fields needed for public deliveries
+rename(us1990, "DO-PSDel.x" = "do-psdel")
+rename(us1995, "DO-PSDel.y" = "PS-DelDO")
+
+rename(us1990, "STATEFIPS" = "scode")
+rename(us1995, "STATEFIPS" = "StateCode")
+
 domestic1 <- merge(us2010, us2015, by='FIPS') 
-domestic1 <- select(domestic1, "FIPS", "DO-PSPCp.x", "DO-PSPCp.y")
+domestic2 <- merge(us2005, us2010, by='FIPS') 
+domestic3 <- merge(us1990, us1995, by='FIPS') 
+
+domestic1 <- select(domestic1, "FIPS", "DO-PSPCp.x", "DO-PSPCp.y", STATEFIPS.x, STATEFIPS.y)
 # select only counties with positive public deliveries
 domestic1 <- subset(domestic1, domestic1$`DO-PSPCp.x` > 0)
 domestic1$diff <- (domestic1$`DO-PSPCp.y`/ domestic1$`DO-PSPCp.x`)
 domestic1$time <- 5
 mean(domestic1$diff)
 
-domestic2 <- merge(us2005, us2010, by='FIPS') 
 domestic2$'DO-PSPCp.x' <- domestic2$`DO-PSDel.x` / domestic2$`PS-TOPop.x`
 domestic2$'DO-PSPCp.y' <- domestic2$`DO-PSDel.y` / domestic2$`PS-TOPop.y`
-domestic2 <- select(domestic2, "FIPS", "DO-PSPCp.x", "DO-PSPCp.y")
+domestic2 <- select(domestic2, "FIPS", "DO-PSPCp.x", "DO-PSPCp.y", STATEFIPS.x, STATEFIPS.y)
 # select only counties with positive public deliveries
 domestic2 <- subset(domestic2, domestic2$`DO-PSPCp.x` > 0)
 domestic2 <- subset(domestic2, domestic2$`DO-PSPCp.y` > 0)
@@ -59,12 +68,9 @@ domestic2$diff <- (domestic2$`DO-PSPCp.y`/ domestic2$`DO-PSPCp.x`)
 domestic2$time <- 10
 mean(domestic2$diff)
 
-rename(us1990, "DO-PSDel.x" = "do-psdel")
-rename(us1995, "DO-PSDel.y" = "PS-DelDO")
-domestic3 <- merge(us1990, us1995, by='FIPS') 
 domestic3$'DO-PSPCp.x' <- domestic3$`do-psdel` / domestic3$`ps-popto`
 domestic3$'DO-PSPCp.y' <- domestic3$`DO-PSDel` / domestic3$`DO-PSPop`
-domestic3 <- select(domestic3, "FIPS", "DO-PSPCp.x", "DO-PSPCp.y")
+domestic3 <- select(domestic3, "FIPS", "DO-PSPCp.x", "DO-PSPCp.y", STATEFIPS.x, STATEFIPS.y)
 # select only counties with positive public deliveries
 domestic3 <- subset(domestic3, domestic3$`DO-PSPCp.x` > 0)
 domestic3 <- subset(domestic3, domestic3$`DO-PSPCp.y` > 0)
@@ -80,6 +86,3 @@ annualr.1 <- mean(domestic1$diff)**(1/5) - 1
 annualr.2 <- mean(domestic2$diff)**(1/5) - 1
 annualr.3 <- mean(domestic3$diff)**(1/5) - 1
 
-domestic <- rbind(domestic1, domestic2, domestic3)
-d.rate <- lm(diff ~ log(time), data=domestic)
-summary(d.rate)
