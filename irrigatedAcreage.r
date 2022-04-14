5de80e02b046166065e5efa8fe627f90796bd563
twarzin <twwarziniack@fs.fed.us>
2022-04-14 13:27:38 -0600

    Update irrigatedAcreage.r

--- irrigatedAcreage.r
+++ irrigatedAcreage.r
@@ -4,6 +4,12 @@
 
 rm(list = ls())  # clears memory
 
+library(tidyr)
+library(ggplot2)
+library(reshape2)
+library(dplyr)  # Has the pipe operator %>%.
+library(data.table)
+
 # for Travis desktop:
 setwd("D:/5_RPA/Demand model")
 acre.data <- read.csv('1_BaseData/acredata-updated.csv', header=TRUE)
@@ -29,12 +35,79 @@ acres <- rename(acre.data,
 df <- merge(acres, pop.inc)
 
 df2015 <- df[df$year == 2015, ] 
+df2016 <- df[df$year == 2016, ] 
+df2017 <- df[df$year == 2017, ] 
+df2018 <- df[df$year == 2018, ] 
+df2019 <- df[df$year == 2019, ] 
+df2020 <- df[df$year == 2020, ] 
+df2021 <- df[df$year == 2021, ] 
+df2022 <- df[df$year == 2022, ] 
+df2023 <- df[df$year == 2023, ] 
+df2024 <- df[df$year == 2024, ] 
+df2025 <- df[df$year == 2025, ] 
+df2026 <- df[df$year == 2026, ] 
+df2027 <- df[df$year == 2027, ] 
+df2028 <- df[df$year == 2028, ] 
+df2029 <- df[df$year == 2029, ] 
+df2030 <- df[df$year == 2030, ] 
+df2031 <- df[df$year == 2031, ] 
+df2032 <- df[df$year == 2032, ] 
+df2033 <- df[df$year == 2033, ] 
+df2034 <- df[df$year == 2034, ] 
+df2035 <- df[df$year == 2035, ] 
+df2036 <- df[df$year == 2036, ] 
+df2037 <- df[df$year == 2037, ] 
+df2038 <- df[df$year == 2038, ] 
+df2039 <- df[df$year == 2039, ] 
+df2040 <- df[df$year == 2040, ] 
+df2041 <- df[df$year == 2041, ] 
+df2042 <- df[df$year == 2042, ] 
+df2043 <- df[df$year == 2043, ] 
+df2044 <- df[df$year == 2044, ] 
+df2045 <- df[df$year == 2045, ] 
+df2046 <- df[df$year == 2046, ] 
+df2047 <- df[df$year == 2047, ] 
+df2048 <- df[df$year == 2048, ] 
+df2049 <- df[df$year == 2049, ] 
+df2050 <- df[df$year == 2050, ] 
+df2051 <- df[df$year == 2051, ] 
+df2052 <- df[df$year == 2052, ] 
+df2053 <- df[df$year == 2053, ] 
+df2054 <- df[df$year == 2054, ] 
+df2055 <- df[df$year == 2055, ] 
+df2056 <- df[df$year == 2056, ] 
+df2057 <- df[df$year == 2057, ] 
+df2058 <- df[df$year == 2058, ] 
+df2059 <- df[df$year == 2059, ] 
+df2060 <- df[df$year == 2060, ] 
+df2061 <- df[df$year == 2061, ] 
+df2062 <- df[df$year == 2062, ] 
+df2063 <- df[df$year == 2063, ] 
+df2064 <- df[df$year == 2064, ] 
+df2065 <- df[df$year == 2065, ] 
+df2066 <- df[df$year == 2066, ] 
+df2067 <- df[df$year == 2067, ] 
+df2068 <- df[df$year == 2068, ] 
+df2069 <- df[df$year == 2069, ] 
+df2070 <- df[df$year == 2070, ] 
+
+# projections are every five years
 df2015$acres <- df2015$ac2015
+df2020$acres <- df2020$ac2020
+df2025$acres <- df2025$ac2025
+df2030$acres <- df2030$ac2030
+df2035$acres <- df2035$ac2035
+df2040$acres <- df2040$ac2040
+df2045$acres <- df2045$ac2045
+df2050$acres <- df2050$ac2050
+df2055$acres <- df2055$ac2055
+df2060$acres <- df2060$ac2060
+df2065$acres <- df2065$ac2065
+df2070$acres <- df2070$ac2070
 
 
 
 
-df2$acres <- mapply(df1$acres, function(x) if(df1$year == 2015) return(1*df1$ac2015))
 
 
 else if (df$year == 2016) {df$pr_acre_2015+1*(df$pr_acre_2020-df$pr_acre_2015)/5}
