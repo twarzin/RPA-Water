1ad0f1ce0963293dcbe67d09f686f2b9d0a65c83
twarzin <twwarziniack@fs.fed.us>
2022-04-14 21:26:27 -0600

    adding years
    adding acreage for all years and binding dataframes

--- irrigatedAcreage.r
+++ irrigatedAcreage.r
@@ -105,45 +105,122 @@ df2060$acres <- df2060$ac2060
 df2065$acres <- df2065$ac2065
 df2070$acres <- df2070$ac2070
 
+df2016$acres <- df$pr_acre_2015+1*(df$pr_acre_2020-df$pr_acre_2015)/5
+df2017$acres <- df$pr_acre_2015+2*(df$pr_acre_2020-df$pr_acre_2015)/5
+df2018$acres <- df$pr_acre_2015+3*(df$pr_acre_2020-df$pr_acre_2015)/5
+df2019$acres <- df$pr_acre_2015+4*(df$pr_acre_2020-df$pr_acre_2015)/5
 
+df2021$acres <- df$pr_acre_2020+1*(df$pr_acre_2025-df$pr_acre_2020)/5
+df2022$acres <- df$pr_acre_2020+2*(df$pr_acre_2025-df$pr_acre_2020)/5
+df2023$acres <- df$pr_acre_2020+3*(df$pr_acre_2025-df$pr_acre_2020)/5
+df2024$acres <- df$pr_acre_2020+4*(df$pr_acre_2025-df$pr_acre_2020)/5
 
+df2026$acres <- df$pr_acre_2025+1*(df$pr_acre_2030-df$pr_acre_2025)/5
+df2027$acres <- df$pr_acre_2025+2*(df$pr_acre_2030-df$pr_acre_2025)/5
+df2028$acres <- df$pr_acre_2025+3*(df$pr_acre_2030-df$pr_acre_2025)/5
+df2029$acres <- df$pr_acre_2025+4*(df$pr_acre_2030-df$pr_acre_2025)/5
 
+df2031$acres <- df$pr_acre_2030+1*(df$pr_acre_2035-df$pr_acre_2030)/5
+df2032$acres <- df$pr_acre_2030+2*(df$pr_acre_2035-df$pr_acre_2030)/5
+df2033$acres <- df$pr_acre_2030+3*(df$pr_acre_2035-df$pr_acre_2030)/5
+df2034$acres <- df$pr_acre_2030+4*(df$pr_acre_2035-df$pr_acre_2030)/5
 
+df2036$acres <- df$pr_acre_2035+1*(df$pr_acre_2040-df$pr_acre_2035)/5
+df2037$acres <- df$pr_acre_2035+2*(df$pr_acre_2040-df$pr_acre_2035)/5
+df2038$acres <- df$pr_acre_2035+3*(df$pr_acre_2040-df$pr_acre_2035)/5
+df2039$acres <- df$pr_acre_2035+4*(df$pr_acre_2040-df$pr_acre_2035)/5
 
-else if (df$year == 2016) {df$pr_acre_2015+1*(df$pr_acre_2020-df$pr_acre_2015)/5}
-else if (df$year == 2017) {df$pr_acre_2015+2*(df$pr_acre_2020-df$pr_acre_2015)/5}
-else if (df$year == 2018) {df$pr_acre_2015+3*(df$pr_acre_2020-df$pr_acre_2015)/5}
-else if (df$year == 2019) {df$pr_acre_2015+4*(df$pr_acre_2020-df$pr_acre_2015)/5}
+df2041$acres <- df$pr_acre_2040+1*(df$pr_acre_2045-df$pr_acre_2040)/5
+df2042$acres <- df$pr_acre_2040+2*(df$pr_acre_2045-df$pr_acre_2040)/5
+df2043$acres <- df$pr_acre_2040+3*(df$pr_acre_2045-df$pr_acre_2040)/5
+df2044$acres <- df$pr_acre_2040+4*(df$pr_acre_2045-df$pr_acre_2040)/5
 
-else if (df$year == 2020) {df$pr_acre_2020} 
-else if (df$year == 2021) {df$pr_acre_2020+1*(df$pr_acre_2025-df$pr_acre_2020)/5}
-else if (df$year == 2022) {df$pr_acre_2020+2*(df$pr_acre_2025-df$pr_acre_2020)/5}
-else if (df$year == 2023) {df$pr_acre_2020+3*(df$pr_acre_2025-df$pr_acre_2020)/5}
-else if (df$year == 2024) {df$pr_acre_2020+4*(df$pr_acre_2025-df$pr_acre_2020)/5}
+df2046$acres <- df$pr_acre_2045+1*(df$pr_acre_2050-df$pr_acre_2045)/5
+df2047$acres <- df$pr_acre_2045+2*(df$pr_acre_2050-df$pr_acre_2045)/5
+df2048$acres <- df$pr_acre_2045+3*(df$pr_acre_2050-df$pr_acre_2045)/5
+df2049$acres <- df$pr_acre_2045+4*(df$pr_acre_2050-df$pr_acre_2045)/5
 
-else if (df$year == 2025) {df$pr_acre_2025}
-else if (df$year == 2026) {df$pr_acre_2025+1*(df$pr_acre_2030-df$pr_acre_2025)/5}
-else if (df$year == 2027) {df$pr_acre_2025+2*(df$pr_acre_2030-df$pr_acre_2025)/5}
-else if (df$year == 2028) {df$pr_acre_2025+3*(df$pr_acre_2030-df$pr_acre_2025)/5}
-else if (df$year == 2029) {df$pr_acre_2025+4*(df$pr_acre_2030-df$pr_acre_2025)/5}
+df2051$acres <- df$pr_acre_2050+1*(df$pr_acre_2055-df$pr_acre_2050)/5
+df2052$acres <- df$pr_acre_2050+2*(df$pr_acre_2055-df$pr_acre_2050)/5
+df2053$acres <- df$pr_acre_2050+3*(df$pr_acre_2055-df$pr_acre_2050)/5
+df2054$acres <- df$pr_acre_2050+4*(df$pr_acre_2055-df$pr_acre_2050)/5
 
-else if (df$year == 2030) {df$pr_acre_2030}
-else if (df$year == 2031) {df$pr_acre_2030+1*(df$pr_acre_2035-df$pr_acre_2030)/5}
-else if (df$year == 2032) {df$pr_acre_2030+2*(df$pr_acre_2035-df$pr_acre_2030)/5}
-else if (df$year == 2033) {df$pr_acre_2030+3*(df$pr_acre_2035-df$pr_acre_2030)/5}
-else if (df$year == 2034) {df$pr_acre_2030+4*(df$pr_acre_2035-df$pr_acre_2030)/5}
+df2056$acres <- df$pr_acre_2055+1*(df$pr_acre_2060-df$pr_acre_2055)/5
+df2057$acres <- df$pr_acre_2055+2*(df$pr_acre_2060-df$pr_acre_2055)/5
+df2058$acres <- df$pr_acre_2055+3*(df$pr_acre_2060-df$pr_acre_2055)/5
+df2059$acres <- df$pr_acre_2055+4*(df$pr_acre_2060-df$pr_acre_2055)/5
 
-else if (df$year == 2035) {df$pr_acre_2035}
-else if (df$year == 2036) {df$pr_acre_2035+1*(df$pr_acre_2040-df$pr_acre_2035)/5}
-else if (df$year == 2037) {df$pr_acre_2035+2*(df$pr_acre_2040-df$pr_acre_2035)/5}
-else if (df$year == 2038) {df$pr_acre_2035+3*(df$pr_acre_2040-df$pr_acre_2035)/5}
-else if (df$year == 2039) {df$pr_acre_2035+4*(df$pr_acre_2040-df$pr_acre_2035)/5}
+df2061$acres <- df$pr_acre_2060+1*(df$pr_acre_2065-df$pr_acre_2060)/5
+df2062$acres <- df$pr_acre_2060+2*(df$pr_acre_2065-df$pr_acre_2060)/5
+df2063$acres <- df$pr_acre_2060+3*(df$pr_acre_2065-df$pr_acre_2060)/5
+df2064$acres <- df$pr_acre_2060+4*(df$pr_acre_2065-df$pr_acre_2060)/5
 
-else if (df$year == 2040) {df$pr_acre_2040}
-else if (df$year == 2045) {df$pr_acre_2045}
-else if (df$year == 2050) {df$pr_acre_2050}
-else if (df$year == 2055) {df$pr_acre_2055}
-else if (df$year == 2060) {df$pr_acre_2060}
-else if (df$year == 2065) {df$pr_acre_2065}
-else if (df$year == 2070) {df$pr_acre_2070}
+df2066$acres <- df$pr_acre_2065+1*(df$pr_acre_2070-df$pr_acre_2065)/5
+df2067$acres <- df$pr_acre_2065+2*(df$pr_acre_2070-df$pr_acre_2065)/5
+df2068$acres <- df$pr_acre_2065+3*(df$pr_acre_2070-df$pr_acre_2065)/5
+df2069$acres <- df$pr_acre_2065+4*(df$pr_acre_2070-df$pr_acre_2065)/5
+
+dfall <- rbind(df2015, 
+      df2016,
+      df2017,
+      df2018,
+      df2019,
+
+      df2020,
+      df2021,
+      df2022,
+      df2023,
+      df2024,
+      df2025,
+      df2026,
+      df2027,
+      df2028,
+      df2029,
+      
+      df2030,
+      df2031,
+      df2032,
+      df2033,
+      df2034,
+      df2035,
+      df2036,
+      df2037,
+      df2038,
+      df2039,
+      
+      df2040,
+      df2041,
+      df2042,
+      df2043,
+      df2044,
+      df2045,
+      df2046,
+      df2047,
+      df2048,
+      df2049,
+      
+      df2050,
+      df2051,
+      df2052,
+      df2053,
+      df2054,
+      df2055,
+      df2056,
+      df2057,
+      df2058,
+      df2059,
+      
+      df2060,
+      df2061,
+      df2062,
+      df2063,
+      df2064,
+      df2065,
+      df2066,
+      df2067,
+      df2068,
+      df2069,
+      
+      df2070)
+ 
 
