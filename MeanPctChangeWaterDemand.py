75a72fe3b65efc05cf8622c56e7a863d55eff1ad
Pam Froemke <pam.froemke@usda.gov>
2022-10-31 15:20:43 -0600

    Update MeanPctChangeWaterDemand.py
    Continued script development.
    SQL finally works, adding functions for pivot table, percent change calculations.

--- MeanPctChangeWaterDemand.py
+++ MeanPctChangeWaterDemand.py
@@ -8,8 +8,8 @@ Date:           2022
 Updates:
 Description:    Calculate mean percent change in water demand from 2015 to 2070
                 across all scenarios.
-                    1. Group by
-Required Args:
+                    1. Create pivot table of raw csv data.
+Required Args:  Input data, format = ID is FIPS+Scenario, 2015 wd, 2070 wd.
 Optional Args:
 Notes:
 """
@@ -41,17 +41,16 @@ inputTable = os.path.join(
 # Raw water demand data imported to the gdb
 outputTable = os.path.join(
     GDB,
-    'ConsWithdrawal')
-# Sectors of interest (domestic and thermoelectric)
+    'Withdrawals')
+
+# Sectors and scenarios of interest (domestic and thermoelectric;
+#   RCP 4.5 and 8.5.)
 sectorList = [
     'dp',
     'th']
-rcpList = [
+scenarioList = [
     '45',
     '85']
-# Subset table for DP, RCP 4.5
-tableDP45 = \
-    'WithdrawalDP45'
 
 # Set the workspace.
 env.workspace = GDB
@@ -61,40 +60,72 @@ try:
         'Starting analysis for calculating mean percent change '
         'in water demand:')
     
-    # # Import the CSV raw data table to the gdb.
-    # print(
-    #     '    Importing the raw data...')
-    # arcpy.conversion.ExportTable(
-    #     inputTable,
-    #     outputTable)
+    # Import the CSV raw data table to the gdb.
+    print(
+        '    Importing the raw data...')
+    fieldMap = r'fips "fips" true true false 6 Text 0 0,First,#,E:\_Projects\WaterDemand\WaterDemandProject\DataWaterDemand\MeanPctChange\cons_and_withdrawal.csv,fips,-1,-1;sector "sector" true true false 8000 Text 0 0,First,#,E:\_Projects\WaterDemand\WaterDemandProject\DataWaterDemand\MeanPctChange\cons_and_withdrawal.csv,sector,0,8000;scenario "scenario" true true false 8000 Text 0 0,First,#,E:\_Projects\WaterDemand\WaterDemandProject\DataWaterDemand\MeanPctChange\cons_and_withdrawal.csv,scenario,0,8000;yearr "yearr" true true false 4 Text 0 0,First,#,E:\_Projects\WaterDemand\WaterDemandProject\DataWaterDemand\MeanPctChange\cons_and_withdrawal.csv,yearr,-1,-1;withdrawal "withdrawal" true true false 8 Double 0 0,First,#,E:\_Projects\WaterDemand\WaterDemandProject\DataWaterDemand\MeanPctChange\cons_and_withdrawal.csv,withdrawal,-1,-1'
+    arcpy.conversion.TableToTable(
+        inputTable,
+        outputTable,
+        field_mapping=fieldMap)
+    
+    # Add a concatenated ID field.
+    fullName = "!fips! + !scenario!"
 
-    # For loops:
     # Select the sectors.
     for sct in sectorList:
-        print(f'    Processing sector {sct}')
-        #   Select the dp sector for RCP 4.5.
-        #   Note - the 'year' field name is misspelled 'yearr' in the raw data.
-        print(
-            '    Selecting sector and scenario...')
-        '"sector" = \'4\''
-        '"sector" = ' + sct
-        for rcp in rcpList:
-            sqlDP45 = \
-                "sector = sct " \
-                "And scenario LIKE rcp " \
-                "And yearr = 2015 " \
-                "Or sector = sct " \
-                "And scenario LIKE rcp " \
-                "And yearr = 2070"
+        for rcp in scenarioList:
+            print(
+                f"Selecting records for sector '{sct}' "
+                f"and scenarios for 'RCP {rcp}'...")
+            # Table name example: Withdrawals_dp45
+            tableSubset = \
+                'Withdrawals_' + sct + rcp
+            # Select statement:
+            #   sector = sct
+            #   And scenario LIKE rcp
+            #   And yearr = 2015
+            #   Or sector = sct
+            #   And scenario LIKE rcp
+            #   And yearr = 2070
+            sql = \
+                "sector = '" + sct + \
+                "' And scenario LIKE '%" + rcp + \
+                "%' And yearr = 2015 Or sector = '" + sct + \
+                "' And scenario LIKE '%" + rcp + "%' And yearr = 2070"
+            # The correct sql prints out like this:
+            # sector = 'dp' And scenario LIKE '%45%' And yearr =
+            #   2015 Or sector = 'dp' And scenario LIKE '%45%' And yearr = 2070
+            print(
+                f"    The sql statement is: {sql}")
             arcpy.analysis.TableSelect(
-                outputTable,
-                tableDP45,
-                sqlDP45)
-    
-        #       For each county, calculate % change from 2015 to 2070.
-        #       ((2070wd-2015wd)/2015wd)*100
-        #   Repeat for dp 8.5, th 4.5, and th 8.5.
-    
+                in_table=outputTable,
+                out_table=tableSubset,
+                where_clause=sql)
+            # Transpose the table.
+            
+            # Add a field for calculating the percent change.
+            #   ((2070wd-2015wd)/2015wd)*100
+            arcpy.management.AddField(
+                in_table=tableSubset,
+                field_name="PctChange",
+                field_type='DOUBLE',
+                field_alias="Pct Change")
+            # Calculate percent change from 2015 to 2070.
+            # Calculate the mean percent change for all records in the table.
+            statsFields = [["PctChange", "MEAN"]]
+            tableFinal = tableSubset + "_mean"
+            arcpy.analysis.Statistics(
+                in_table=tableSubset,
+                out_table=tableFinal,
+                statistics_fields=statsFields,
+                case_field='ID')
+            
+    # Clean up
+    # Remove intermediate data tables?
+    #   Could simply leave as is and use the four tables, or compile them into
+    #   one table and then remove the four.
+
     print('Done!')
 
 except arcpy.ExecuteError as e:
